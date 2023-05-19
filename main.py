0bec52d1bdcfed2d1e21059c961f798190a0b5c1
juliakwyvent <125362238+juliakwyvent@users.noreply.github.com>
2023-05-19 07:26:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"Restaurant {self.restaurant_name} has {self.cuisine_type} cuisine.")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is now open!")
+
+
+newRestaurant = Restaurant("Pizza House", "Italian")
+print(newRestaurant.restaurant_name)
+print(newRestaurant.cuisine_type)
+
+newRestaurant.describe_restaurant()
+newRestaurant.open_restaurant()
