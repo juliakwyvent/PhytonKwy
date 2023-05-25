469805eaa5319141e59cdc4b247a2e17f1f9dbfd
juliakwyvent <125362238+juliakwyvent@users.noreply.github.com>
2023-05-25 15:01:04 +0300

    Update 3 лаба 3.1.py

--- 3 лаба 3.1.py
+++ 3 лаба 3.1.py
@@ -1,11 +1,7 @@
 N = int(input("Введите количество слов: "))
-words = []
-for i in range(N):
-    word = input(f"Введите слово {i + 1}: ")
-    words.append(word)
 result = ""
-for word in words:
+for i in range(N):
+    word = input("Введите слово")
     result += word + " "
 result = result.rstrip()
-
 print(result)
